ddc685c8791234851eea263424e6f2dc25ad366d
waldener <2173656118@qq.com>
2021-04-11 16:41:12 +0800

    删除多余组件

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -10,7 +10,6 @@
     <el-card>
       <!-- 警告区域 -->
       <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon :closable="false"></el-alert>
-
       <!-- 选择商品分类区域 -->
       <el-row class="cat_opt">
         <el-col>
